ca8dc3f7784b8fec55a3de7f3afb4a411fea1fbb
codecodecode333 <gksrnr2305@gmail.com>
2026-07-04 19:51:18 +0900

    feat: add building definition asset

---
+++ Assets/Scripts/Construction/BuildingDefinition.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(
+    menuName = "Construction/Building Definition",
+    fileName = "BD_NewBuilding")]
+
+public class BuildingDefinition : ScriptableObject
+{
+    [SerializeField] private string displayName;
+    [SerializeField] private GameObject buildingPrefab;
+}
